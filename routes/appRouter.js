867d4bf875ffc2e62488db15959791652a9d6492
nandweza <allannandweza@gmail.com>
2023-03-24 19:34:59 +0300

    created post page, with more updates

--- routes/appRouter.js
+++ routes/appRouter.js
@@ -2,8 +2,14 @@ const express = require('express');
 const router = express.Router();
 
 
+let posts = [];
+
 router.get('/', (req, res) => {
     res.render('index');
+});
+
+router.get('/post', (req, res) => {
+    res.render('post');
 });
 
 module.exports = router;
